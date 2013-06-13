7a3cd21e4a25b279bd9b18ae9825a5ef78e96106
Ronald <selsery@gmail.com>
2013-06-13 15:55:05 +0200

    just trolling the repo

--- features/support/headless.rb
+++ features/support/headless.rb
@@ -1,14 +1,14 @@
-if ENV['BUILD_NUMBER'].present?
-  require 'headless'
-  require 'selenium-webdriver'
- 
-  headless = Headless.new(display: 100)
-  headless.start
- 
-  at_exit do
-    headless.destroy
-  end
-
-  #Capybara.javascript_driver = :selenium
-
-end
+#if ENV['BUILD_NUMBER'].present?
+#  require 'headless'
+#  require 'selenium-webdriver'
+# 
+#  headless = Headless.new(display: DISPLAY_PORT)
+#  headless.start
+# 
+#  at_exit do
+#    headless.destroy
+#  end
+#
+#  #Capybara.javascript_driver = :selenium
+#
+#end
